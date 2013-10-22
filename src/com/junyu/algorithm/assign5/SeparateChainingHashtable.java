c3aca2b42236570d4935551cbe46e7b9b1cce35b
Eageon <yujun.eageon@gmail.com>
2013-10-21 21:26:58 -0500

    Separate Chaining completed

--- src/com/junyu/algorithm/assign5/SeparateChainingHashtable.java
+++ src/com/junyu/algorithm/assign5/SeparateChainingHashtable.java
@@ -1,5 +1,6 @@
 package com.junyu.algorithm.assign5;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class SeparateChainingHashtable {
@@ -10,19 +11,36 @@ public class SeparateChainingHashtable {
 	public SeparateChainingHashtable(int size) {
 		tableSize = size;
 		chainList = new LinkedList[tableSize];
-		for(int i = 0; i < chainList.length;i++) {
-			chainList[i]=new LinkedList<>();
+		for (int i = 0; i < chainList.length; i++) {
+			chainList[i] = new LinkedList<>();
 		}
 	}
-	
+
 	public SeparateChainingHashtable() {
 		this(DEFAULT_HASHTABLE_SIZE);
 	}
-	
-	public static int hash(int value) {
+
+	private int hash(int value) {
 		int hashVal = 0;
-		
-		
-		return 0;
+		hashVal = value % tableSize;
+		return hashVal;
+	}
+
+	public void insert(int value) {
+		LinkedList<Integer> whichList = chainList[hash(value)];
+
+		if (whichList.contains(value)) {
+			whichList.add(value);
+		}
+	}
+
+	public void showTable() {
+		for (int i = 0; i < chainList.length; i++) {
+			System.out.print("Table " + i + ": ");
+			Iterator<Integer> itor = chainList[i].iterator();
+			while(itor.hasNext())
+				System.out.print(itor.next() + " ");
+			System.out.println("");
+		}
 	}
 }
